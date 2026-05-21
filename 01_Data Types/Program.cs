450e9c252f41e903422712ffd69d516a414ab78b
CrystalxBlade <satish141m@gmail.com>
2026-05-21 21:12:22 +0530

    Data Type Created

---
+++ 01_Data Types/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Data_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome Blade");
+            Console.ReadLine();
+        }
+    }
+}
