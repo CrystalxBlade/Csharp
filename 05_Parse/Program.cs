f81aff09293afbcaaa1b33806dcac82c93ec356d
CrystalxBlade <satish141m@gmail.com>
2026-05-27 15:29:45 +0530

    Parsing

--- 05_Parse/Program.cs
+++ 05_Parse/Program.cs
@@ -10,7 +10,32 @@ namespace _05_Parse
     {
         static void Main(string[] args)
         {
+            /*
+             
+            //  Parse
+              
+            string s = Console.ReadLine();
+            int num = int.Parse(s);
+            Console.WriteLine(num);
+            
 
+            // TryParse
+
+            string s = Console.ReadLine();
+            
+            if(int.TryParse(s, out int n))
+            {
+                Console.WriteLine(n);
+            }
+            else
+            {
+                Console.WriteLine("Parsing failed");
+            }
+
+            */
+
+
+            Console.ReadLine();
         }
     }
 }
