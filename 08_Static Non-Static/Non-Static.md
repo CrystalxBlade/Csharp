a5673e07421e6f7ae583e458505f13adea1d7dac
CrystalxBlade <satish141m@gmail.com>
2026-05-29 14:58:34 +0530

    Non Static

---
+++ 08_Static Non-Static/Non-Static.md
@@ -0,0 +1,23 @@
+ 
+ ```
+ class Blade
+    {
+        public void Player()
+        {
+            Console.WriteLine("Player health 90% ");
+        }
+    }
+
+ class Program
+    {
+        static void Main(string[] args)
+        {
+            Blade b = new Blade();
+            b.Player();
+        }
+
+    }
+```
+
+### We need to create an instance to call the non static function
+
--- 08_Static Non-Static/Program.cs
+++ 08_Static Non-Static/Program.cs
@@ -6,6 +6,13 @@ using System.Threading.Tasks;
 
 namespace _08_Static_Non_Static
 {
+    class Blade
+    {
+        public void Player()
+        {
+            Console.WriteLine("Player health 90% ");
+        }
+    }
     class Testing
     {
         public static void something()
@@ -17,9 +24,13 @@ namespace _08_Static_Non_Static
     {
         static void Main(string[] args)
         {
+            Blade b = new Blade();
+            b.Player();
+
             Testing.something();
 
             Welcome();
+            Console.ReadLine();
         }
 
         static void Welcome()
