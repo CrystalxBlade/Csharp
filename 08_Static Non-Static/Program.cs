48b0f632c2e3527e12e69fcc3e1d5b4857d72cad
CrystalxBlade <satish141m@gmail.com>
2026-05-29 14:24:12 +0530

    Static and Non Static

---
+++ 08_Static Non-Static/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Static_Non_Static
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
