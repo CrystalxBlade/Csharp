6a85f4c8403eef476d1eddae653bb363de91e4fd
CrystalxBlade <satish141m@gmail.com>
2026-05-31 22:22:20 +0530

    Ref

--- 06_Ref_n_Out/Program.cs
+++ 06_Ref_n_Out/Program.cs
@@ -10,9 +10,19 @@ namespace _06_Ref_n_Out
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Ref out");
+            int num = 20;
+
+            something(ref num);
+
+            Console.WriteLine(num);
 
             Console.ReadLine();
+        }
+
+        static void something(ref int a)
+        {
+            a += 10;
+            Console.WriteLine($" method value = {a}");
         }
     }
 }
