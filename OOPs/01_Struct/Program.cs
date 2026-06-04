ef825747476d4afa652d6cf4f8996359834671fd
CrystalxBlade <satish141m@gmail.com>
2026-06-04 20:37:19 +0530

    Person

--- OOPs/01_Struct/Program.cs
+++ OOPs/01_Struct/Program.cs
@@ -30,8 +30,13 @@ namespace _01_Struct
         }
     }
 
-    */
+    
 
+    ------------------------------------------------------------------
+
+
+    
+      
     struct Person
     {
         public string name;
@@ -67,6 +72,49 @@ namespace _01_Struct
             person.birthMonth = birthMonth;
 
             return person;
+        }
+    }
+
+    */
+
+    struct Person
+    {
+        public string name;
+        public int age;
+        public int birthMonth;
+
+        public Person(string name, int age, int birthMonth)
+        {
+            this.name = name;
+            this.age = age;
+            this.birthMonth = birthMonth;
+        }
+    }
+
+    class Crystal
+    {
+        static void Main(string[] args)
+        {
+            Person p = Details();
+
+            Console.WriteLine($"{p.name} - {p.age} - {p.birthMonth}");
+
+            Console.ReadLine();
+        }
+
+        static Person Details()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter your age: ");
+            int age =  Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter your birth month: ");
+            int birthMonth = Convert.ToInt32(Console.ReadLine());
+
+
+            return new Person(name, age, birthMonth);
         }
     }
 }
