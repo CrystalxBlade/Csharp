a4353afde23d37a823df36da85e756c926dd7d27
CrystalxBlade <satish141m@gmail.com>
2026-06-04 20:19:50 +0530

    Struct

--- OOPs/01_Struct/Program.cs
+++ OOPs/01_Struct/Program.cs
@@ -6,11 +6,14 @@ using System.Threading.Tasks;
 
 namespace _01_Struct
 {
+    /*
+    
     struct Person
     {
         public string name;
         public int age;
     }
+    
 
     class Program
     {
@@ -20,6 +23,50 @@ namespace _01_Struct
 
             person.name = "Blade";
             person.age = 24;
+
+            Console.WriteLine($"{person.name} - {person.age}");
+
+            Console.ReadLine();
+        }
+    }
+
+    */
+
+    struct Person
+    {
+        public string name;
+        public int age;
+        public int birthMonth;
+    }
+
+    class Crystal
+    {
+        static void Main(string[] args)
+        {
+            Person p = Details();
+            Console.WriteLine($"{p.name} - {p.age} - {p.birthMonth}");
+
+            Console.ReadLine();
+        }
+
+        static Person Details()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter your birth month: ");
+            int birthMonth = Convert.ToInt32(Console.ReadLine());
+
+            Person person;
+
+            person.name = name;
+            person.age = age;
+            person.birthMonth = birthMonth;
+
+            return person;
         }
     }
 }
