5cf1250bf6a7e56fe5cc56c0b67abbcce13d8b6b
CrystalxBlade <satish141m@gmail.com>
2026-06-05 20:39:27 +0530

    Encapsulation

--- OOPs/02_Class Constructor/Program.cs
+++ OOPs/02_Class Constructor/Program.cs
@@ -20,13 +20,36 @@ namespace _02_Class_Constructor
         {
             this.name = name;
             this.age = age;
-            Console.WriteLine($"Parameterized Constructor {name} {age}");
+            Console.WriteLine($"Parameterized Constructor: {name} {age}");
+        }
+
+        public Crystal(Crystal obj)
+        {
+            this.name = obj.name;
+            this.age = obj.age;
+            Console.WriteLine($"Copyt Constructor: {name} {age}");
         }
         
         static void Main(string[] args)
         {
             Crystal c = new Crystal();
             Crystal c1 = new Crystal("Blade", 24);
+            Crystal c2 = new Crystal(c1);
+
+            Console.WriteLine();
+
+            Console.WriteLine("--- After Copy ---");
+            Console.WriteLine($"c1 -> name: {c1.name} age: {c1.age}");
+            Console.WriteLine($"c2 -> name: {c2.name} age: {c2.age}");
+
+            Console.WriteLine();
+            Console.WriteLine("--- Modify c2 ---");
+
+            c2.name = "Crystal";
+            c2.age = 23;
+
+            Console.WriteLine($"c1 -> name: {c1.name} age: {c1.age}");
+            Console.WriteLine($"c2 -> name: {c2.name} age: {c2.age}");
 
             Console.ReadLine();
         }
---
+++ OOPs/03_Encapsulation/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Encapsulation
+{
+    class Program
+    {
+        private int atmPin;
+
+        public int getValue()
+        {
+            return atmPin;
+        }
+
+        public void setValue(int pin)
+        {
+            atmPin = pin;
+        }
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.atmPin = 1234;
+            p.getValue();
+        }
+    }
+}
