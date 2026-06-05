d80349655fd1749373d441db9c1d70b71eecf533
CrystalxBlade <satish141m@gmail.com>
2026-06-05 16:19:16 +0530

    Class

---
+++ OOPs/02_Class Constructor/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Class_Constructor
+{
+    class Crystal
+    {
+        public string name;
+        public int age;
+
+        public Crystal()
+        {
+            Console.WriteLine("Default Constructor");
+        }
+
+        public Crystal(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+            Console.WriteLine("Default Constructor");
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
