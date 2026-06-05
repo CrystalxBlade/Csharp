17ce6109fbeb697fd042f821e6bb0edef7de06e6
CrystalxBlade <satish141m@gmail.com>
2026-06-05 21:10:42 +0530

    Bank

--- OOPs/03_Encapsulation/Program.cs
+++ OOPs/03_Encapsulation/Program.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 
 namespace _03_Encapsulation
 {
+    /*
+     
     class Program
     {
         private int atmPin;
@@ -19,12 +21,112 @@ namespace _03_Encapsulation
         {
             atmPin = pin;
         }
+    }
 
+    class Emp
+    {
         static void Main(string[] args)
         {
             Program p = new Program();
-            p.atmPin = 1234;
-            p.getValue();
+            p.setValue(4321);
+
+            Console.WriteLine($"{p.getValue()}");
+
+            Console.ReadLine();
+        }
+    }
+
+    */
+
+    class BankAccount
+    {
+        private string ownerName;
+        private double balance;
+        private string password;
+
+        public BankAccount(string ownerName, int balance, string password)
+        {
+            this.ownerName = ownerName;
+            this.balance = balance;
+            this.password = password;
+        }
+
+        public void Deposit(double amount)
+        {
+            if(amount <= 0)
+            {
+                Console.WriteLine("Deposit amount must be greater than 0");
+            }
+            else
+            {
+                balance = balance + amount;
+            }
+        }
+        public void WithDraw(double amount, string enterPassword)
+        {
+            if(enterPassword != password)
+            {
+                Console.Write("Wrong password - Access Denied");
+                Console.WriteLine();
+            }
+            else if(amount > balance)
+            {
+                Console.WriteLine("Insufficient balance");
+            }
+            else if(amount <= 0)
+            {
+                Console.WriteLine("Withdrawl amount must be greater than 0");
+            }
+            else
+            {
+                balance = balance - amount;
+                Console.WriteLine($"Withdrawl amount: {amount} - remaining balance: {balance}");
+            }
+        }
+        public void CheckBalance(string enterPassword)
+        {
+            if(enterPassword != password)
+            {
+                Console.WriteLine("Wrong password - Acces denied");
+            }
+            else
+            {
+                Console.WriteLine($"Account Owner: {ownerName}");
+                Console.WriteLine($"Current balance: {balance}");
+            }
+        }
+    }
+
+    class Person
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount("Blade", 5000, "blade123");
+
+            Console.WriteLine("---Deposit---");
+            account.Deposit(2000);
+
+            Console.WriteLine();
+
+            Console.WriteLine("---Check Balance---");
+            account.CheckBalance("blade123");
+
+            Console.WriteLine();
+
+            Console.WriteLine("--- Withdraw with wrong password---");
+            account.WithDraw(2000, "worngPassword");
+
+            Console.WriteLine();
+
+            Console.WriteLine("--- Withdraw with correct password---");
+            account.WithDraw(2000, "blade123");
+
+            Console.WriteLine();
+
+            Console.WriteLine("---Final Balance---");
+            account.CheckBalance("blade123");
+
+            Console.ReadLine();
         }
     }
 }
