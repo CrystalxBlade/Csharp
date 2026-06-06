e04fee78fb548d5bf2cae73ed132ddf711434d52
CrystalxBlade <satish141m@gmail.com>
2026-06-06 16:28:00 +0530

    Hierarchical

--- OOPs/04_Inheritance/Program.cs
+++ OOPs/04_Inheritance/Program.cs
@@ -34,5 +34,80 @@ namespace _04_Inheritance
             Console.ReadLine();
         }
     }
+
+
+           -------------- Multilevel Inheritance --------------
+    
+
+    class Crystal
+    {
+        public void Disp1()
+        {
+            Console.WriteLine("Crystal");
+        }
+    }
+    class Blade : Crystal
+    {
+        public void Disp2()
+        {
+            Console.WriteLine("Blade");
+        }
+    }
+    class Skull : Blade
+    {
+        public void Disp3()
+        {
+            Console.WriteLine("Skull");
+        }
+
+        static void Main(string[] args)
+        {
+            Skull s = new Skull();
+            s.Disp1();
+            s.Disp2();
+            s.Disp3();
+
+            Console.ReadLine();
+        }
+    }
+
     */
+
+    class Crystal
+    {
+        public void Hornet()
+        {
+            Console.WriteLine("Hornet is Dashing");
+        }
+    }
+    class Skull : Crystal
+    {
+        public void Knight()
+        {
+            Console.WriteLine("Knight is slashing");
+        }
+    }
+    class Blade : Crystal
+    {
+        public void Player()
+        {
+            Console.WriteLine("Blade is Playing");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Skull s = new Skull();
+            s.Hornet();               // Inherited from Crystal
+            s.Knight();               // Own Method
+
+            Blade b = new Blade();
+            b.Hornet();               // Inherited form Crystal
+            b.Player();               // Own Method
+
+            Console.ReadLine();
+        }
+    }
 }
