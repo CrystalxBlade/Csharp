807074cc2675ab2962a5cf0328c96fd2069925f9
CrystalxBlade <satish141m@gmail.com>
2026-06-07 14:06:06 +0530

    Interface

---
+++ OOPs/05_Interface/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_Interface
+{
+    interface IPrey
+    {
+        void Flee();
+    }
+
+    interface IPredator
+    {
+        void Hunt();
+    }
+    class Rabbit : IPrey
+    {
+        public void Flee()
+        {
+            Console.WriteLine("The Rabbit runs away");
+        }
+    }
+    class Hawk
+    {
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rabbit r = new Rabbit();
+            r.Flee();
+
+            Console.ReadLine();
+        }
+    }
+}
