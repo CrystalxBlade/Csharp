7e574e920cc3160d46674ddc01a71f8a4d49135e
CrystalxBlade <satish141m@gmail.com>
2026-06-08 19:56:06 +0530

    Abstract Class

--- OOPs/07_Abstract/Program.cs
+++ OOPs/07_Abstract/Program.cs
@@ -6,11 +6,25 @@ using System.Threading.Tasks;
 
 namespace _07_Abstract
 {
+    abstract class Animal
+    {
+        public abstract void MakeSound();
+    }
+    class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Woof! Woof");
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Dog d = new Dog();
+            d.MakeSound();
 
+            Console.ReadLine();
         }
     }
 }
