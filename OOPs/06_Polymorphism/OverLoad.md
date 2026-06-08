eb5ecb9db9aa65ec2ffac27c392be8a22978b246
CrystalxBlade <satish141m@gmail.com>
2026-06-08 19:17:59 +0530

    OverLoadind

---
+++ OOPs/06_Polymorphism/OverLoad.md
@@ -0,0 +1,25 @@
+﻿## ✅ Method Overloading
+
+**Method Overloading** is a feature of **Compile-Time Polymorphism** in C#.
+
+It allows you to **define multiple methods** with the **same name** but **different parameter lists**.
+
+---
+
+### 📌 Why use Method Overloading?
+
+- Improves **readability** and **reusability**.
+- Lets you **perform similar operations** with **different input types or numbers**.
+- Cleaner code, no need to create different method names like `AddInt()`, `AddDouble()`, etc.
+
+---
+
+### 🧠 Rules for Method Overloading
+
+You can overload a method by changing:
+
+1. Number of parameters
+2. Type of parameters
+3. Order of parameters
+
+❌ You **cannot** overload just by changing the method name or return type.
---
+++ OOPs/06_Polymorphism/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_Polymorphism
+{
+    class Program
+    {
+        void Add(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+
+        void Add(int a, int b, int c)
+        {
+            Console.WriteLine($"Overload by number of parameters: {a + b + c}");
+        }
+        void Add(string name, float x)
+        {
+            Console.WriteLine($"Overload by type of parameters:{name} : {x}");
+        }
+        void Add(float x, string name)
+        {
+            Console.WriteLine($"Overload by order of parameters: {x} : {name}");
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Add(2, 4);
+            p.Add(2, 4, 6);
+            p.Add("Blade", 24);
+            p.Add(24, "Blade");
+
+            Console.ReadLine();
+        }
+    }
+}
