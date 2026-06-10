276a9aab9f6df6a1a7a4c508c0dea2d49cb4feda
CrystalxBlade <satish141m@gmail.com>
2026-06-10 15:57:55 +0530

    Cars

--- Array/Array/Array/Program.cs
+++ Array/Array/Array/Program.cs
@@ -10,7 +10,13 @@ namespace Array
     {
         static void Main(string[] args)
         {
+            string[] cars = { "BMW", "Mustang", "Corvette" };
 
+            Console.WriteLine(cars[0]);
+            Console.WriteLine(cars[1]);
+            Console.WriteLine(cars[2]);
+
+            Console.ReadLine();
         }
     }
 }
