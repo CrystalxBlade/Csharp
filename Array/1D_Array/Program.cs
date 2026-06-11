62e7b8c8a7f620bf74cd0ee78c364f754d8bb1bd
CrystalxBlade <satish141m@gmail.com>
2026-06-11 16:25:44 +0530

    Max

--- Array/1D_Array/Program.cs
+++ Array/1D_Array/Program.cs
@@ -10,7 +10,37 @@ namespace _1D_Array
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("1D Array");
+            /*
+             
+            int[] arr = {2, 4, 6, 8};
+            int sum = 0;
+
+            int i = 0;
+            while(i < arr.Length)
+            {
+                sum += arr[i];
+                i++;
+            }
+
+            Console.Write($"Sum of arr is: {sum}");
+
+            */
+
+            int[] arr = { 2, 4, 6, 8, 10, 5, 1};
+
+            int max = arr[0];
+
+            for(int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+            }
+
+            Console.Write($"Maximum value is : {max}");
+
+            Console.ReadLine();
         }
     }
 }
