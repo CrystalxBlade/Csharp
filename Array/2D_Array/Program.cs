06e465a23813d22ea8733584781cee04f90bc25b
CrystalxBlade <satish141m@gmail.com>
2026-06-14 21:09:56 +0530

    2D matrix

--- Array/2D_Array/Program.cs
+++ Array/2D_Array/Program.cs
@@ -10,6 +10,8 @@ namespace _2D_Array
     {
         static void Main(string[] args)
         {
+            /*
+              
             int[,] matrix = new int[3, 2];
 
             matrix[0, 0] = 1;
@@ -18,6 +20,58 @@ namespace _2D_Array
             matrix[1, 1] = 4;
             matrix[2, 0] = 5;
             matrix[2, 1] = 6;
+
+            for(int i = 0; i < 3; i++)
+            {
+                for(int j = 0; j < 2; j++)
+                {
+                    Console.Write($"{matrix[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+
+            -------------------------------------------------------
+
+            int[,] matrix = new int[4, 3];
+
+            matrix[0, 0] = 1;
+            matrix[0, 1] = 2;
+            matrix[0, 2] = 3;
+            matrix[1, 0] = 4;
+            matrix[1, 1] = 5;
+            matrix[1, 2] = 6;
+            matrix[2, 0] = 7;
+            matrix[2, 1] = 8;
+            matrix[2, 2] = 9;
+            matrix[3, 0] = 10;
+            matrix[3, 1] = 11;
+            matrix[3, 2] = 12;
+
+            for(int i = 0; i < 4; i++)
+            {
+                for(int j = 0; j < 3; j++)
+                {
+                    Console.Write($"{matrix[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+
+            -------------------------------------------------------
+
+            int[,] matrix = {{1,2,3} , {4,5,6} , {7,8,9} };
+
+            for(int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for(int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write($"{matrix[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+            
+            */
+
+            Console.ReadLine();
         }
     }
 }
