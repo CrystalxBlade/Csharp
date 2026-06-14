b2546f52459c6da6c2d3ff4ad8c3369a5d3e14e0
CrystalxBlade <satish141m@gmail.com>
2026-06-14 14:52:45 +0530

    Insertion Sort

--- Array/1D_Array/Program.cs
+++ Array/1D_Array/Program.cs
@@ -221,10 +221,31 @@ namespace _1D_Array
              ----------------- Insertion Sort -----------------
         */
 
+        static void Main(string[] args)
+        {
+            int[] arr = { 4, 1, 5, 2, 3 };
+            int n = arr.Length;
 
-        Console.ReadLine();
+            for(int i = 1; i < n; i++)
+            {
+                int curr = arr[i];
+                int prev = i - 1;
+
+                while(prev >= 0 && arr[prev] > curr)
+                {
+                    arr[prev + 1] = arr[prev];
+                    prev--;
+                }
+
+                arr[prev + 1] = curr;
+            }
+
+            foreach(int num in arr)
+            {
+                Console.Write($"{num} ");
+            }
+            Console.ReadLine();
         }
-        
     }
 
 }
