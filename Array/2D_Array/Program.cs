c4aed1772059ac6385245fb1d70fd15dd2863256
CrystalxBlade <satish141m@gmail.com>
2026-06-17 20:32:48 +0530

    Input/Print 2D

--- Array/2D_Array/Program.cs
+++ Array/2D_Array/Program.cs
@@ -133,6 +133,51 @@ namespace _2D_Array
             return false;
         }
 
+        -------------------- Input/Print with method 2D ---------------------
+        
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter rows: ");
+            int r = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter columns: ");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            int[,] matrix = new int[r, c];
+
+            Input(matrix);
+            Print(matrix);
+
+            Console.ReadLine();
+
+        }
+
+        static void Input(int[,] matrix)
+        {
+            for(int i = 0;  i < matrix.GetLength(0); i++)
+            {
+                for(int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write($"{i}, {j}: ");
+                    matrix[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void Print(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write($"{matrix[i,j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
         */
     }
 }
