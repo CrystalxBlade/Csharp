75afa41d41f4311a69b435308c6c399ad9a77afc
CrystalxBlade <satish141m@gmail.com>
2026-06-20 15:09:05 +0530

    Lists

---
+++ Lists/List/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List
+{
+    class Program
+    {
+        // List = datat structures that represents the list of objects that can be accessed by index.
+        //        Similar to array, but can dynamically increase/ decrease in size
+        //        using System. Collections.Generic;
+        static void Main(string[] args)
+        {
+            /*
+             
+             ----------------- Add / Remove -----------------
+
+            List<String> food = new List<String>();
+
+            food.Add("Pizza");
+            food.Add("Hamburger");
+            food.Add("Hotdog");
+            food.Add("Fries");
+
+            food.Remove("Fries");
+
+            ----------------- Insert -----------------
+            */
+
+            List<String> food = new List<String>();
+
+            foreach (string item in food)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
