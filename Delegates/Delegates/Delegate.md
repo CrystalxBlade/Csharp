da7337a39a2162a3575d9ad4dbe00a85a95c0251
CrystalxBlade <satish141m@gmail.com>
2026-06-21 22:20:12 +0530

    Invoke

--- Delegates/Delegates/Delegate.md
+++ Delegates/Delegates/Delegate.md
@@ -3,7 +3,14 @@
 ### A delegates holds the reference of a method and then calls the method for execution.
 
 
-To call a method by using a delegate we have 3 steps:
+## To call a method by using a delegate we have 3 steps:
 
-1. Define a delegate
-[<modifiers>] delegate void <Name>([<parameter list>])  
+### 1. Define a delegate
+### [<modifiers>] delegate void <Name>([<parameter list>])
+
+- public delegate void AddDelegate(int a, int b); // return type should be same as method
+
+public void AddNums(int a, int b); // Normal method 
+
+- public delegate string SayDelegate(string str); // no need to use static when method is already static
+public static string SayHellp(string name);
--- Delegates/Delegates/Program.cs
+++ Delegates/Delegates/Program.cs
@@ -6,11 +6,33 @@ using System.Threading.Tasks;
 
 namespace Delegates
 {
+    public delegate void AddDelegate(int a, int b);
+    public delegate string SayDelegate(string str);
+
     class Program
     {
+        public void AddNums(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+
+        public static string SayHello(string name)
+        {
+            return $"Hello {name}";
+        }
+
         static void Main(string[] args)
         {
 
+            Program p = new Program();
+            AddDelegate ad = new AddDelegate(p.AddNums);
+            ad.Invoke(2, 4);
+
+            SayDelegate sd = new SayDelegate(Program.SayHello);
+            string str = sd.Invoke("Blade");
+            Console.WriteLine(str);
+
+            Console.ReadLine();
         }
     }
 }
