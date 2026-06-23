43c667a253a1467c38a3707f7fefa8774230c2a7
CrystalxBlade <satish141m@gmail.com>
2026-06-23 17:26:45 +0530

    Anonymous

---
+++ Delegates/Anonymous/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anonymous
+{
+    public delegate string GreetingsDelegate(string name);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GreetingsDelegate obj = delegate (string name)
+            {
+                return $"Hello {name} very good morning";
+            };
+
+            string str = obj.Invoke("Blade");
+
+            Console.WriteLine(str);
+
+            Console.ReadLine();
+        }
+    }
+}
