691ca99a37a075949d5c710843fdad83667e2f7d
CrystalxBlade <satish141m@gmail.com>
2026-06-24 19:34:57 +0530

    Lambda

---
+++ Delegates/Lambda/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda
+{
+    public delegate string GreetingsDelegate(string name);
+    class Program
+    {
+        public static string Greetings(string name)
+        {
+            return $"Hello {name} very good morning";
+        }
+        static void Main(string[] args)
+        {
+            GreetingsDelegate obj = new GreetingsDelegate(Greetings);
+
+            string str = obj.Invoke("Blade");
+            Console.WriteLine(str);
+
+            Console.ReadLine();
+        }
+    }
+}
