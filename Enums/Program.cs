c9276b55c7e728617e10cc4dfcbdafacf77a4e36
CrystalxBlade <satish141m@gmail.com>
2026-06-25 18:36:08 +0530

    DaysEnum

--- Enums/Program.cs
+++ Enums/Program.cs
@@ -8,8 +8,46 @@ namespace Enums
 {
     class Program
     {
+        public enum Days
+        {
+            Monday = 2,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday = 69,
+            Sunday
+        }
+
         static void Main(string[] args)
         {
+            Days d = Days.Wednesday;
+
+            Console.WriteLine($"Today is {d}");
+            Console.WriteLine($"Numeric value is: {(int)d}");
+
+            Console.WriteLine("----------------------------");
+
+            // Printing values of all the Enums
+
+            foreach (int i in Enum.GetValues(typeof(Days)))
+                Console.WriteLine(i);
+
+            Console.WriteLine("----------------------------");
+
+            // Printing names of all the Enums
+
+            foreach (string n in Enum.GetNames(typeof(Days)))
+                Console.WriteLine(n);
+
+            Console.WriteLine("----------------------------");
+
+            // Printing name with values of all the Enums
+
+            foreach (int i in Enum.GetValues(typeof(Days)))
+                Console.WriteLine($"{i} : {(Days)i}");
+
+            Console.ReadLine();
         }
     }
 }
