277104c5d98f1aed7ce97716cce695134314b480
CrystalxBlade <satish141m@gmail.com>
2026-06-25 17:58:53 +0530

    Enums

--- Delegates/Lambda/Program.cs
+++ Delegates/Lambda/Program.cs
@@ -9,13 +9,12 @@ namespace Lambda
     public delegate string GreetingsDelegate(string name);
     class Program
     {
-        public static string Greetings(string name)
-        {
-            return $"Hello {name} very good morning";
-        }
         static void Main(string[] args)
         {
-            GreetingsDelegate obj = new GreetingsDelegate(Greetings);
+            GreetingsDelegate obj = (name) =>
+            {
+                return $"Hello {name} very good morning";
+            };
 
             string str = obj.Invoke("Blade");
             Console.WriteLine(str);
