854272cf4647ac18d1c2f5c77732d369d84708cb
CrystalxBlade <satish141m@gmail.com>
2026-06-29 16:31:52 +0530

    Events with Delegates

--- Events/Events and Delegates/Program.cs
+++ Events/Events and Delegates/Program.cs
@@ -6,10 +6,40 @@ using System.Threading.Tasks;
 
 namespace Events_and_Delegates
 {
+    public delegate void DoorEventHandler();
+
+    class Door
+    {
+        public event DoorEventHandler OnDoorOpened;
+
+        public void OpenDoor()
+        {
+            Console.WriteLine("Door is opened");
+
+            OnDoorOpened?.Invoke();
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            Door door = new Door();
+
+            door.OnDoorOpened += LightsOn;
+            door.OnDoorOpened += WelcomeSound;
+
+            door.OpenDoor();
+            Console.ReadLine();
+        }
+
+        static void LightsOn()
+        {
+            Console.WriteLine("Lights turned on");
+        }
+        static void WelcomeSound()
+        {
+            Console.WriteLine("Welcome sound played");
         }
     }
 }
