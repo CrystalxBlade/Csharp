18e408a3fd97cbc35a91cbf7b3eb502bf3682469
CrystalxBlade <satish141m@gmail.com>
2026-06-30 17:10:22 +0530

    Exception Handling

--- Exception Handling/Exception/Program.cs
+++ Exception Handling/Exception/Program.cs
@@ -4,12 +4,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Exception
+namespace ExceptionHandling
 {
     class Program
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Console.Write("Enter the first number: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter the secong number: ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+                if (num2 == 0)
+                {
+                    throw new DivideByZeroException("Second number cannot be Zero.");
+                }
+
+                int result = num1 / num2;
+                Console.WriteLine($"Result = {result}");
+            }
+
+            catch(FormatException)
+            {
+                Console.WriteLine("Please enter only number.");
+            }
+            catch(DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Unexpected Error: + {ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Program Finished.");
+            }
+            Console.ReadLine();
         }
+        
     }
 }
