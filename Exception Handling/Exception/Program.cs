25b8e3c6a7ac49da50fe402d0c68fe50eee5b6c9
CrystalxBlade <satish141m@gmail.com>
2026-06-30 20:33:07 +0530

    Finally

--- Exception Handling/Exception/Program.cs
+++ Exception Handling/Exception/Program.cs
@@ -15,7 +15,7 @@ namespace ExceptionHandling
                 Console.Write("Enter the first number: ");
                 int num1 = Convert.ToInt32(Console.ReadLine());
 
-                Console.Write("Enter the secong number: ");
+                Console.Write("Enter the second number: ");
                 int num2 = Convert.ToInt32(Console.ReadLine());
 
                 if (num2 == 0)
