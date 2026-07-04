32804f68282e46467c59d134e43f919a9b89c8f2
CrystalxBlade <satish141m@gmail.com>
2026-07-04 22:40:20 +0530

    HashTable

---
+++ Collections/Hashtable/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace HashTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable stud = new Hashtable();
+
+            stud.Add(101, "Blade");
+            stud.Add(105, "Knight");
+            stud.Add(107, "Hornet");
+            stud.Add("Email", "hornet@gmail.com");
+            stud.Add("Coins", 2500);
+
+            foreach(DictionaryEntry students in stud)
+            {
+                Console.WriteLine($"{students.Key} : {students.Value}");
+            }
+
+            Console.WriteLine($"\nContains key 105: {stud.ContainsKey(105)}");
+
+            stud.Remove(105);
+
+            Console.WriteLine("\nAfter Removing:");
+
+            foreach (DictionaryEntry students in stud)
+            {
+                Console.WriteLine($"{students.Key} : {students.Value}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
