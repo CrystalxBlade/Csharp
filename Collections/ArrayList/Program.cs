ec1f06b0d19ccdb05caaea1933428818347cfe5f
CrystalxBlade <satish141m@gmail.com>
2026-07-04 13:49:11 +0530

    Capacity

---
+++ Collections/ArrayList/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList al = new ArrayList();
+
+            al.Add(100);
+            Console.WriteLine(al.Capacity);
+
+            Console.ReadLine();
+        }
+    }
+}
