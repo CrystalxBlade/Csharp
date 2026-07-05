30b4ae06ff9d4c0e455f2c835735e17776c118e0
CrystalxBlade <satish141m@gmail.com>
2026-07-05 19:31:49 +0530

    Dictionary

---
+++ Collections/Dictionary/Dictionary.md
@@ -0,0 +1,9 @@
+| Hashtable                     | Dictionary<TKey, TValue>                      |
+| ----------------------------- | --------------------------------------------- |
+| Legacy collection             | Modern generic collection                     |
+| Introduced in .NET 1.0        | Introduced in .NET 2.0                        |
+| Stores `object`               | Stores specific types (`int`, `string`, etc.) |
+| Not type-safe                 | Type-safe                                     |
+| Requires casting              | No casting needed                             |
+| Slower due to boxing/unboxing | Faster                                        |
+| Rarely used in new code       | Used in almost every modern C# application    |
---
+++ Collections/Dictionary/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> students = new Dictionary<int, string>();
+
+            students.Add(6, "Blade");
+            students.Add(8, "Hornet");
+            students.Add(4, "Knight");
+
+            foreach(var stud in students)
+            {
+                Console.WriteLine($"{stud.Key} : {stud.Value}");
+            }
+
+            Console.WriteLine($"\nConstains key 8 : {students.ContainsKey(8)}");
+
+            students.Remove(8);
+
+            Console.WriteLine("\nAfter removing: \n");
+
+            foreach(var stud in students)
+            {
+                Console.WriteLine($"{stud.Key} : {stud.Value}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
