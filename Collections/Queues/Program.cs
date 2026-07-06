5faca6598e7f74b040098658d8146f39936ee7c6
CrystalxBlade <satish141m@gmail.com>
2026-07-06 11:07:43 +0530

    Queue

---
+++ Collections/Queues/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Queues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> customers = new Queue<string>();
+
+            customers.Enqueue("Blade");
+            customers.Enqueue("Hornet");
+            customers.Enqueue("Knight");
+
+            Console.WriteLine($"Served : {customers.Dequeue()}");
+            Console.WriteLine($"Next customer: {customers.Peek()}");
+
+
+            Console.WriteLine("\nWaiting customers \n");
+
+            foreach(string customer in customers)
+            {
+                Console.WriteLine(customer);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Collections/Queues/Queue.md
@@ -0,0 +1,17 @@
+### Queue is the opposite of Stack.
+
+### While a Stack follows LIFO (Last In, First Out), a Queue follows FIFO (First In, First Out).
+
+### Think of it as people waiting in line.
+
+## What is a Queue?
+
+### Imagine you're standing in line at a movie theater.
+
+```
+Person A
+Person B
+Person C
+Person D
+
+```
