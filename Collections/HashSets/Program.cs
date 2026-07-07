33fc47ab6141a46203c25bf0c8108d8286c56ba6
CrystalxBlade <satish141m@gmail.com>
2026-07-07 19:13:41 +0530

    hash

--- Collections/HashSets/Program.cs
+++ Collections/HashSets/Program.cs
@@ -10,7 +10,29 @@ namespace HashSets
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("HashSet");
+            HashSet<int> numbers = new HashSet<int>();
+
+            numbers.Add(10);
+            numbers.Add(20);
+            numbers.Add(30);
+            numbers.Add(10);
+
+            foreach(int numb in numbers)
+            {
+                Console.WriteLine(numb);
+            }
+
+            Console.WriteLine($"\nContains 20: {numbers.Contains(20)}");
+
+            numbers.Remove(20);
+
+            Console.WriteLine("\nAfter Removing");
+
+            foreach(int numb in numbers)
+            {
+                Console.WriteLine(numb);
+            }
+
             Console.ReadLine();
         }
     }
