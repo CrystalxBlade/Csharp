6aed4c1c813e98f1da5b25617c1128a43f9a52c9
CrystalxBlade <satish141m@gmail.com>
2026-07-08 12:24:16 +0530

    Generic Class

--- Collections/Generic Class/Program.cs
+++ Collections/Generic Class/Program.cs
@@ -1,15 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Generic_Class
 {
+    class Box<T>
+    {
+        public T Value { get; set; }
+
+        public void Display()
+        {
+            Console.WriteLine(Value);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Box<int> number = new Box<int>();
+            number.Value = 100;
+            number.Display();
+
+            Box<string> name = new Box<string>();
+            name.Value = "Blade";
+            name.Display();
+
+            Console.ReadLine();
         }
     }
 }
