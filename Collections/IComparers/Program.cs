c8c73c8e6bb6ab2d5ab891bd9cae652f9c3560fc
CrystalxBlade <satish141m@gmail.com>
2026-07-09 20:57:09 +0530

    IComparers

--- Collections/IComparers/Program.cs
+++ Collections/IComparers/Program.cs
@@ -1,15 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace IComparers
 {
+    class Student
+    {
+        public string Name { get; set; }
+        public int Marks { get; set; }
+    }
+
+    class SortByName : IComparer<Student>
+    {
+        public int Compare(Student x, Student y)
+        {
+            return x.Name.CompareTo(y.Name);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            List<Student> stud = new List<Student>()
+            {
+
+            new Student { Name = "Crystal", Marks = 99 },
+            new Student { Name = "Hornet", Marks = 79 },
+            new Student { Name = "Blade", Marks = 69 },
+            new Student { Name = "Knight", Marks = 89 },
+
+            };
+
+            stud.Sort(new SortByName());
+
+            foreach(var student in stud)
+            {
+                Console.WriteLine($"{student.Name} - {student.Marks}");
+            }
+
+            Console.ReadLine();
         }
     }
 }
