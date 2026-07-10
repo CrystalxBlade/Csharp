0ea4ee5ac4b1e0970da83c2b5701c4fffaf7d3b8
CrystalxBlade <satish141m@gmail.com>
2026-07-10 19:47:27 +0530

    LINQ

--- LINQ/Linq/Program.cs
+++ LINQ/Linq/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Linq
 {
@@ -10,6 +7,7 @@ namespace Linq
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Linq");
         }
     }
 }
