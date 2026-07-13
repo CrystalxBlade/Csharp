ffa4e274b4e6480f316b44908e5b8b9eddb3d528
CrystalxBlade <satish141m@gmail.com>
2026-07-13 21:22:18 +0530

    where

--- LINQ/Linq/Program.cs
+++ LINQ/Linq/Program.cs
@@ -4,11 +4,37 @@ using System.Linq;
 
 namespace Linq
 {
+    class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Marks { get; set; }
+
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Linq");
+            List<Student> students = new List<Student>()
+            {
+                new Student{ID = 1, Name = "Blade", Age = 24, Marks = 80},
+                new Student{ID = 2, Name = "Hornet", Age = 22, Marks = 95},
+                new Student{ID = 3, Name = "Knight", Age = 23, Marks = 70},
+                new Student{ID = 4, Name = "Crystal", Age = 21, Marks = 85}
+            };
+
+            // Where
+            Console.WriteLine("WHERE");
+            var whenResult = students.Where(s => s.Marks >= 80);
+
+            foreach(var student in whenResult)
+            {
+                Console.WriteLine($"{student.Name} - {student.Marks}");
+            }
+
+            Console.ReadLine();
         }
     }
 }
