250ebf81c417dda1277185200b872e83f9813820
CrystalxBlade <satish141m@gmail.com>
2026-07-17 17:33:04 +0530

    Addes IEnumerable

---
+++ IEnumerables/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IEnumerables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("IEnumerable");
+            Console.ReadLine();
+        }
+    }
+}
