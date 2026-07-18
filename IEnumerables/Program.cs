0c2c5030a7f0438706a5298a5eedd2d4f668ddf5
CrystalxBlade <satish141m@gmail.com>
2026-07-18 20:10:30 +0530

    IEnumerable

--- IEnumerables/Program.cs
+++ IEnumerables/Program.cs
@@ -6,12 +6,39 @@ using System.Threading.Tasks;
 
 namespace IEnumerables
 {
+    class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Marks { get; set; }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("IEnumerable");
+            List<Student> students = new List<Student>()
+            {
+                new Student{ID = 1, Name = "Blade", Marks = 69},
+                new Student{ID = 2, Name = "Crystal", Marks = 79}
+            };
+
+
+            /*
+             =================================================
+              1. List<T> can be assigned to IEnumerable<T>
+             =================================================
+            */
+
+            Console.WriteLine("===== List To IEnumerable =====");
+            IEnumerable<Student> result = students;
+
+            foreach(Student student in result)
+            {
+                Console.WriteLine(student.Name);
+            }
             Console.ReadLine();
         }
+
     }
 }
