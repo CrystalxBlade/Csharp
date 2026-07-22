5ab276789a85ed225d7299c261795fcca5f3f3a0
CrystalxBlade <satish141m@gmail.com>
2026-07-22 05:42:10 +0530

    IsEven

--- Extension Method/Program.cs
+++ Extension Method/Program.cs
@@ -8,10 +8,28 @@ namespace Extension_Method
 {
     public static class MyExtension
     {
+        // Example 1: Extension Method for string
+
         public static void Print(this string text)
         {
             Console.WriteLine(text);
         }
+
+        // Example 2: Extension Method with Return Value
+
+        public static string ReverseText(this string text)
+        {
+            char[] chars = text.ToCharArray();
+            Array.Reverse(chars);
+
+            return new string(chars);
+        }
+
+        public static bool IsEven(this int num)
+        {
+            return num % 2 == 0;
+        }
+
     }
 
     class Program
@@ -27,6 +45,28 @@ namespace Extension_Method
             name.Print();
 
             Console.WriteLine();
+
+            //====================================
+            // Example 2: ReverseText()
+            //====================================
+
+            Console.WriteLine("===== Reverse Text =====");
+
+            string text = "Crystal";
+
+            Console.WriteLine(text.ReverseText());
+
+            Console.WriteLine();
+
+            //====================================
+            // Example 3: IsEven()
+            //====================================
+
+            Console.WriteLine("===== IsEven() =====");
+
+            int num = 10;
+
+            Console.WriteLine(num.IsEven());
 
             Console.ReadLine();
         }
