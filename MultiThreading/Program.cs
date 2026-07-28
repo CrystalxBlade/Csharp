0dd20254a94e4ee9f58d7bf5a8a1d625412d9618
CrystalxBlade <satish141m@gmail.com>
2026-07-28 18:45:54 +0530

    workerThread

--- MultiThreading/Program.cs
+++ MultiThreading/Program.cs
@@ -3,19 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
+
 
 namespace MultiThreading
 {
     class Program
     {
+        static void PrintNumbers()
+        {
+            for(int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"Thread: {i}");
+            }
+        }
+
         static void Main(string[] args)
         {
-            Thread t = Thread.CurrentThread;
-            t.Name = "Main Thread";
-            Console.WriteLine($"Current executing thread is: " +
-                $"{Thread.CurrentThread.Name}");
- 
+            Console.WriteLine("===== Creating Thread ===== \n");
+
+            Thread t = new Thread(PrintNumbers);
+            t.Start();
+
             Console.ReadLine();
         }
     }
