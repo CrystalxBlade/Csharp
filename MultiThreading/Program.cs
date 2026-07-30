abb7236895f2fb71a1520d1896d4a36208620307
CrystalxBlade <satish141m@gmail.com>
2026-07-30 18:31:45 +0530

    raceCondition

--- MultiThreading/Program.cs
+++ MultiThreading/Program.cs
@@ -224,5 +224,36 @@ namespace MultiThreading
         }
 
         */
+
+        class Bank
+        {
+            static int counter = 0;
+
+            static void Increment()
+            {
+                for(int i = 1; i <= 100000; i++)
+                {
+                    counter++;
+                }
+            }
+
+            static void Main(string[] args)
+            {
+                Console.WriteLine("===== Race Condition =====\n");
+
+                Thread t1 = new Thread(Increment);
+                Thread t2 = new Thread(Increment);
+
+                t1.Start();
+                t2.Start();
+
+                t1.Join();
+                t2.Join();
+
+                Console.WriteLine($"Counter = {counter}");
+
+                Console.ReadLine();
+            }
+        }
     }
 }
