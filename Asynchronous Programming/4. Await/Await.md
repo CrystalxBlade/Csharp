822fd95da7d33ddab8dc17bbf19656e5533c9d91
CrystalxBlade <satish141m@gmail.com>
2026-08-06 10:50:36 +0530

    Await

---
+++ Asynchronous Programming/4. Await/Await.md
@@ -0,0 +1,60 @@
+﻿# await Keyword
+
+## Definition:
+### The await keyword pauses an async method until the awaited Task completes.
+
+## Syntax:
+
+```
+await Task.Delay(1000);
+
+// or
+
+await SomeAsyncMethod();
+
+```
+## Purpose:
+- Wait for asynchronous operations.
+- Does not block the current thread.
+- Resumes execution after the Task completes.
+
+## Important:
+- Can only be used inside an async method.
+- Works with Task and Task<T>.
+- Makes asynchronous code easier to read.
+
+
+
+## With await
+
+```
+Start
+
+↓
+
+Wait for Download
+
+↓
+
+Continue
+
+```
+
+## Without await
+
+```
+Start
+
+↓
+
+Start Download
+
+↓
+
+Continue Immediately
+
+↓
+
+Download Finishes Later
+
+```
---
+++ Asynchronous Programming/4. Await/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.Await
+{
+    class Program
+    {
+        static async Task DownloadData()
+        {
+            Console.WriteLine("Download Started...");
+            await Task.Delay(3000);
+            Console.WriteLine("Download Finished");
+        }
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("===== await keyword Method =====\n");
+
+            Console.WriteLine("Before Calling Method");
+
+            await DownloadData();
+
+            Console.WriteLine("After Calling Method");
+
+            Console.ReadLine();
+        }
+    }
+}
