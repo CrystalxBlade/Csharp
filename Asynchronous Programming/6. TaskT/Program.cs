5b6fbb4872a0f8c9be9e68a05ee7ea5a67e88def
CrystalxBlade <satish141m@gmail.com>
2026-08-10 19:50:49 +0530

    THread

--- Asynchronous Programming/6. TaskT/Program.cs
+++ Asynchronous Programming/6. TaskT/Program.cs
@@ -1,15 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+
 
 namespace _6.TaskT
 {
     class Program
     {
+
+        static void Player1()
+        {
+            for(int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(1000);
+            }
+        }
+
+        static void Player2()
+        {
+            for (char ch = 'A'; ch <= 'Z'; ch++)
+            {
+                Console.WriteLine(ch);
+                Thread.Sleep(1000);
+            }
+        }
+
         static void Main(string[] args)
         {
+            Thread t1 = new Thread(Player1);
+            Thread t2 = new Thread(Player2);
+
+            t1.Start();
+            
+
+            t2.Start();
+            
+
+            Console.WriteLine("Main thread Completed...");
+
+            Console.ReadLine();
         }
     }
 }
