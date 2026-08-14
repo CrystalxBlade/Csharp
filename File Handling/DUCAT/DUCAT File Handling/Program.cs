afcb59d204e26332154f864f3a5c09c2bcb53b70
CrystalxBlade <satish141m@gmail.com>
2026-08-14 12:53:30 +0530

    FileStream

--- File Handling/DUCAT/DUCAT File Handling/Program.cs
+++ File Handling/DUCAT/DUCAT File Handling/Program.cs
@@ -1,15 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+
 
 namespace DUCAT_File_Handling
 {
     class Program
     {
+        public void ReadFile()
+        {
+            Console.WriteLine("Enter The File Location/Name");
+
+            string file = "D:\\C# codes\\File Handling\\DUCAT\\Demo " +Console.ReadLine()+".txt";
+
+            //Console.WriteLine(file);
+
+            FileStream fs = new FileStream(file, FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+            sr.BaseStream.Seek(0, SeekOrigin.Begin);
+            string str = sr.ReadLine(); 
+            Console.WriteLine(str);
+        }
+
         static void Main(string[] args)
         {
+            Program p = new Program();
+            p.ReadFile();
+
+            Console.ReadLine();
         }
     }
 }
