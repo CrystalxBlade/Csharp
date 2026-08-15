a69bae19f3bab7b7af3deda058a92d641d63f240
CrystalxBlade <satish141m@gmail.com>
2026-08-15 12:53:45 +0530

    StreamReader

--- File Handling/DUCAT/DUCAT File Handling/Program.cs
+++ File Handling/DUCAT/DUCAT File Handling/Program.cs
@@ -6,18 +6,47 @@ namespace DUCAT_File_Handling
 {
     class Program
     {
+        /*
+         
         public void ReadFile()
         {
             Console.WriteLine("Enter The File Location/Name");
 
-            string file = "D:\\C# codes\\File Handling\\DUCAT\\Demo\\"+Console.ReadLine()+".txt";
+            string file = @"D:\C# codes\File Handling\DUCAT\Demo\"+Console.ReadLine()+".txt";
 
             //Console.WriteLine(file);
 
             FileStream fs = new FileStream(file, FileMode.Open, FileAccess.Read);
             StreamReader sr = new StreamReader(fs);
             sr.BaseStream.Seek(0, SeekOrigin.Begin);
-            string str = sr.ReadLine(); 
+            string str = sr.ReadLine();
+            Console.WriteLine(str);
+        }
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.ReadFile();
+
+            Console.ReadLine();
+        }
+
+        */
+
+
+        public void ReadFile()
+        {
+            Console.Write("Enter File Name: ");
+
+            string file = @"D:\C# codes\File Handling\DUCAT\Demo\"+Console.ReadLine()+".txt";
+
+            FileStream fs = new FileStream(file, FileMode.Open, FileAccess.Read);
+
+            StreamReader sr = new StreamReader(fs);
+
+            sr.BaseStream.Seek(0, SeekOrigin.Begin);
+
+            string str = sr.ReadLine();
             Console.WriteLine(str);
         }
 
