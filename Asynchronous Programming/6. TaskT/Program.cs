6a59800979f3bdf7888ff9c3e1fe15c3c51e25c0
CrystalxBlade <satish141m@gmail.com>
2026-08-18 22:35:58 +0530

    Student

--- Asynchronous Programming/6. TaskT/Program.cs
+++ Asynchronous Programming/6. TaskT/Program.cs
@@ -7,16 +7,11 @@ namespace _6.TaskT
 {
     class Program
     {
-
-        // ================= Task =================
-
         static async Task PrintMessage()
         {
-            await Task.Delay(1000);
+            await Task.Delay(2000);
             Console.WriteLine(" Hello form Task!");
         }
-
-        // ================= Task =================
 
         static async Task<int> GetNumber()
         {
@@ -24,10 +19,27 @@ namespace _6.TaskT
             return 100;
         }
 
+        static async Task<string> GetString()
+        {
+            await Task.Delay(2000);
+            return "Blade";
+        }
+
+        static async Task<Student> GetStudent()
+        {
+            await Task.Delay(2000);
+
+            return new Student
+            {
+                Id = 1,
+                Name = "Crystal",
+                Marks = 69
+            };
+        }
 
         static async Task Main(string[] args)
         {
-            Console.WriteLine("|===== Task<T> ===== | \n");
+            Console.WriteLine("|======= Task<T> ======= | \n");
 
             Console.WriteLine(" ===== Task ===== ");
 
@@ -37,18 +49,42 @@ namespace _6.TaskT
 
            // ========================================
 
-            Console.WriteLine("===== Task<int> =====");
+            Console.WriteLine(" ===== Task<int> =====");
 
             int num = await GetNumber();
 
-            Console.WriteLine($"Number : {num}");
+            Console.WriteLine($" Number : {num}");
 
             Console.WriteLine();
 
             // ========================================
 
-            
+            Console.WriteLine(" ===== Task<string> =====");
+
+            string name = await GetString();
+
+            Console.WriteLine($" Name : {name}");
+
+            Console.WriteLine();
+
+            // ========================================
+
+            Console.WriteLine("===== Task<Student> =====");
+
+            Student stud = await GetStudent();
+
+            Console.WriteLine($"Id : {stud.Id}");
+            Console.WriteLine($"Name : {stud.Name}");
+            Console.WriteLine($"Marks : {stud.Marks}");
+
             Console.ReadLine();
         }
+    }
+
+    class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Marks { get; set; }
     }
 }
